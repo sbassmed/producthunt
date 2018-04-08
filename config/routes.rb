021efd06220da51bdc1bdce72a7f4469cf5714bf
sbassmed <sebastiancamilomarin@gmail.com>
2018-04-08 01:03:49 -0500

    Crea la ruta /products/:id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get '/products', to: 'products#index'
   get '/products/new', to: 'products#new', as: 'new_product'
   post "products", to: 'products#create'
-  get '/products/:id', to: 'products#show'
-  get '/products/:id/edit', to: 'products#edit'
   get '/products/:id', to: 'products#show', as: 'product'
-
-
+  get '/products/:id/edit', to: 'products#edit'
 end
