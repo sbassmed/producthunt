54f9ec3bcec8df41cf2e7d3a1db298488ecae609
sbassmed <sebastiancamilomarin@gmail.com>
2018-04-08 01:18:04 -0500

    Crea la ruta PATCH /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,31 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  def show
+    @product = Product.find(params[:id])
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "El producto fue publicado con éxito"
+    else
+      render :new
+    end
+  end
+  def edit
+    @product = Product.find(params[:id])
+  end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+    else
+      render :edit
+  end
 end
+end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :url, :description)
+  end
