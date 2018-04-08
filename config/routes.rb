729f2c99c8c7ba305b22645cf1c61eb0d980363c
sbassmed <sebastiancamilomarin@gmail.com>
2018-04-08 01:46:13 -0500

    Crea la ruta /products/:id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   patch '/products/:id', to: 'products#update'
+
 end
