2537011bc8367b56bffa8f31f43e26d603f74e4b
sbassmed <sebastiancamilomarin@gmail.com>
2018-04-08 00:24:43 -0500

    Crea la ruta /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,20 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  def show
+    @product = Product.find(params[:id])
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "El producto fue publicado con éxito"
+    else
+      render :new
+    end
+  end
 end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :url, :description)
+  end
